aef98d3f5ef7da40ab5a5846b6701b1e4984e38a
SantinoGiordano <santinogiordano13@gmail.com>
2024-11-04 22:05:28 -0500

    created const list of names

--- src/componets/Names.tsx
+++ src/componets/Names.tsx
@@ -1,24 +1,26 @@
-export function Names(){
-    
-    const names = [ 
-       ' Michael Thompson',
-        'Sarah Bennett',
-        'James Miller',
-        'Emily Harris',
-        'David Robinson',
-        'Jessica Parker',
-        'Matthew Collins',
-        'Olivia Turner',
-        'Christopher Edwards',
-        'Amanda Hughes',
-        'Joshua Clark',
-        'Ashley Scott',
-        'Andrew Morris',
-        'Megan Phillips',
-        'Daniel Ross'
-    ]
-    
-    return(
-        <h1>{names}</h1>
-    )
+export function Names() {
+  const names = [
+    " Michael Thompson",
+    "Sarah Bennett",
+    "James Miller",
+    "Emily Harris",
+    "David Robinson",
+    "Jessica Parker",
+    "Matthew Collins",
+    "Olivia Turner",
+    "Christopher Edwards",
+    "Amanda Hughes",
+    "Joshua Clark",
+    "Ashley Scott",
+    "Andrew Morris",
+    "Megan Phillips",
+    "Daniel Ross",
+  ];
+  names.map((name: string, index: number) => console.log(name, index));
+
+  const namesList: string = names.join("\n");
+  console.log(namesList);
+
+
+  return <div>{namesList}</div>;
 }
