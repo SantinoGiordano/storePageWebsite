8f4d9b3d492bc85485314314b9839f83274fae0e
SantinoGiordano <santinogiordano13@gmail.com>
2024-11-07 20:14:51 -0500

    added new bag icon

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import './styles/backgrounds.css'
 import { MappedData } from './componets/MappedData'
 import { Header } from './componets/Header'
 import { About } from './componets/About'
-
 function App() {
 
   return (
     <>
+  
     <div className='topNavBar'>
-    
+    <img className='burgermenu' src='bag-x.svg' width={"50px"}/>
     </div>
     <div className='background'>
     <Header/>
